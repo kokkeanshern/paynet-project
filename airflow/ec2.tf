25c31325a2d9c45d870cc2c1624617eb592f7ddf
kokkeanshern <kokkeanshern@gmail.com>
2025-05-08 23:52:54 +0800

    update ec2 resource

--- airflow/ec2.tf
+++ airflow/ec2.tf
@@ -27,71 +27,40 @@ data "aws_security_group" "allow_ssh_http" {
 }
 
 resource "aws_instance" "airflow_instance" {
-  ami                    = "ami-08569b978cc4dfa10" # Public Linux
-  instance_type          = "t2.micro"
+  ami                    = "ami-05928b89ce5ea0cf5"
+  instance_type          = "m7g.large"
   key_name = aws_key_pair.airflow-ec2-key-pair.key_name
   subnet_id              = data.aws_subnet.paynet-project-public-subnet.id
   vpc_security_group_ids = [data.aws_security_group.allow_ssh_http.id]
 
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
 
-  # user_data = <<-EOF
-  #             #!/bin/bash
-  #             aws s3 cp s3://my-airflow-assets/docker-compose.yaml ./docker-compose.yaml
-  #             mkdir ./dags ./logs ./plugins
-  #             echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
-  #             sudo yum update -y
-  #             sudo yum install -y docker
-  #             mkdir -p ~/.docker/cli-plugins/
-  #             curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-  #             chmod +x ~/.docker/cli-plugins/docker-compose
-
-  #             sudo service docker start
-  #             sudo usermod -aG docker ec2-user
-  #             docker compose up airflow-init
-  #             docker compose up
-  #             EOF
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = 0.03
+    }
+  }
 
   user_data = <<-EOF
               #!/bin/bash
-              set -e
-
-              # Install Docker and curl
               yum update -y
-              yum install -y docker curl
-
-              # Start Docker
+              yum install -y docker
               service docker start
-              chkconfig docker on
 
-              # Add ec2-user to docker group
-              usermod -aG docker ec2-user
+              curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+              chmod +x /usr/local/bin/docker-compose
 
-              # Install Docker Compose v2 (plugin method)
-              mkdir -p /home/ec2-user/.docker/cli-plugins/
-              curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
-                -o /home/ec2-user/.docker/cli-plugins/docker-compose
-              chmod +x /home/ec2-user/.docker/cli-plugins/docker-compose
-              chown -R ec2-user:ec2-user /home/ec2-user/.docker
-
-              # Create Airflow directories
               mkdir -p /home/ec2-user/dags /home/ec2-user/logs /home/ec2-user/plugins
-              chown -R ec2-user:ec2-user /home/ec2-user
+              cd /home/ec2-user
+              aws s3 cp s3://my-airflow-assets/docker-compose.yaml docker-compose.yaml
+              echo -e "AIRFLOW_UID=$(id -u ec2-user)\nAIRFLOW_GID=0" > .env
+              chown ec2-user:ec2-user .env docker-compose.yaml dags logs plugins
 
-              # Switch to ec2-user for the next steps
-              su - ec2-user -c "
-                # Download Docker Compose file from S3
-                aws s3 cp s3://my-airflow-assets/docker-compose.yaml /home/ec2-user/docker-compose.yaml
+              runuser -l ec2-user -c "sudo docker-compose up airflow-init"
+              runuser -l ec2-user -c "sudo docker-compose up -d"
+              EOF
 
-                # Create .env file for Airflow UID
-                echo -e 'AIRFLOW_UID=50000\nAIRFLOW_GID=0' > /home/ec2-user/.env
-
-                # Start Airflow containers
-                docker compose -f /home/ec2-user/docker-compose.yaml up airflow-init
-                docker compose -f /home/ec2-user/docker-compose.yaml up -d
-              "
-            EOF
-  
   depends_on = [
     aws_s3_object.docker-compose
 
