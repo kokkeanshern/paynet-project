c492d1ad76e55ebbea1ae56875388c2d387b3d89
kokkeanshern <kokkeanshern@gmail.com>
2025-05-11 14:30:23 +0800

    standard dag

--- airflow-dags/test-dag.py
+++ airflow-dags/test-dag.py
@@ -1,11 +1,18 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+def hello_world():
+    print("Hello from Airflow!")
+
 with DAG(
-    dag_id="test-dag",
+    dag_id="safe_test_dag",
     start_date=datetime(2025, 5, 11),
-    schedule="*/2 * * * *",  # OK for 2.8+
+    schedule="*/1 * * * *",  # Every 5 minutes
     catchup=False,
+    tags=["example"],
 ) as dag:
-    EmptyOperator(task_id="task")
+    hello_task = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello_world,
+    )
