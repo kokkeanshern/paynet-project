9ae0609bf6d2ea7c32ecd760bf1ffe528bc10980
kokkeanshern <kokkeanshern@gmail.com>
2025-05-11 14:24:11 +0800

    fix dag

--- airflow-dags/test-dag.py
+++ airflow-dags/test-dag.py
@@ -1,13 +1,11 @@
-import datetime
-
 from airflow import DAG
-from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
 
-# Dummy line
 with DAG(
     dag_id="test-dag",
-    start_date=datetime.datetime(2025, 5, 11),
-    schedule="*/2 * * * *",
-    catchup = False
-):
+    start_date=datetime(2025, 5, 11),
+    schedule="*/2 * * * *",  # OK for 2.8+
+    catchup=False,
+) as dag:
     EmptyOperator(task_id="task")
