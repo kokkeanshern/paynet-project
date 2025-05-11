d89c057e1e779b0c21bac2f6049af2c2b9ea3b5a
kokkeanshern <kokkeanshern@gmail.com>
2025-05-11 13:53:22 +0800

    add new gha workflow

---
+++ airflow-dags/test-dag.py
@@ -0,0 +1,12 @@
+import datetime
+
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="test-dag",
+    start_date=datetime.datetime(2025, 5, 11),
+    schedule="*/2 * * * *",
+    catchup = False
+):
+    EmptyOperator(task_id="task")
